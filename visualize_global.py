91b2e702e2531413c8ef07fa172855711edeb42d
hoaithi <phunghoaithi137@gmail.com>
2025-02-20 22:10:57 +0700

    update

--- visualize_global.py
+++ visualize_global.py
@@ -62,10 +62,10 @@ class VLmae_vis(SCLTransformer):
         )
 
         x, y = text_embeds, image_embeds
-        print(image_embeds.shape)
         t2v_mt = []
         for text_layer, image_layer in zip(self.cross_modal_text_layers, self.cross_modal_image_layers):
             x1 = text_layer(x, y, extend_text_masks, extend_image_masks, output_attentions=True)
+            print(x1.shape)
             y1 = image_layer(y, x, extend_image_masks, extend_text_masks, output_attentions=True)
             x, y = x1[0], y1[0]
             t2v_mt.append(x1[1][0,:,0,1:])
