1e9f3b1664d89ea0b4f9cecfd6ec3cc3d7af3c6f
hoaithi <phunghoaithi137@gmail.com>
2025-03-06 00:59:27 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -256,7 +256,7 @@ _config_vqa_kg = {
     'num_nodes': 1,
     'load_path': "/kaggle/checkpoints/SCL/GLSCL-100k.ckpt",
     'num_workers': 8,
-    'precision': 32,
+    'precision': 16,
     'is_pretrain': False,
 
     # for retrieval
