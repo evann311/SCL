ce4afda2140a93dd810bb6189f1db23504f9ce86
hoaithi <phunghoaithi137@gmail.com>
2025-03-06 23:05:50 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -251,7 +251,7 @@ _config_vqa_kg = {
     # below params varies with the environment
     'data_root': '/kaggle/input/arrow-vqa2',
     'log_dir': "result",
-    'per_gpu_batchsize': 64,  # you should define this manually with per_gpu_batch_size=#
+    'per_gpu_batchsize': 128,  # you should define this manually with per_gpu_batch_size=#
     'num_gpus': 1,
     'num_nodes': 1,
     'load_path': "/kaggle/input/glscl/pytorch/default/1/GLSCL-100k.ckpt",
