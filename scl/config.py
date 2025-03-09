c6dfa06d13c55136043142eff6a12b193419ef47
hoaithi <phunghoaithi137@gmail.com>
2025-03-09 15:57:03 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -347,7 +347,7 @@ _config_vqa_vast = {
     'num_nodes': 1,
     'load_path': "/workspace/glscl/GLSCL-100k.ckpt",
     'num_workers': 32,
-    'precision': 'bf16',
+    'precision': 'bf16-mixed',
     'is_pretrain': False,
 
     # for retrieval
