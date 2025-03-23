b480bd133a0eda7a274c119fef7f09663fdaef33
hoaithi <phunghoaithi137@gmail.com>
2025-03-23 21:59:01 +0700

    update

--- scl/modules/scl_utils.py
+++ scl/modules/scl_utils.py
@@ -1,7 +1,7 @@
 import torch
 import random
 
-from transformers import AdamW
+from torch.optim import AdamW
 from transformers import (
     get_polynomial_decay_schedule_with_warmup,
     get_cosine_schedule_with_warmup,
