3961a0c05d570cbae8d3f96ef2e0ee237e6baa81
hoaithi <phunghoaithi137@gmail.com>
2025-03-24 10:56:09 +0700

    update

--- scl/modules/scl_module.py
+++ scl/modules/scl_module.py
@@ -149,10 +149,7 @@ class SCLTransformer(pl.LightningModule):
 
         # ===================== freeze ======================
         for name, param in self.named_parameters():
-            if "vqa_classifier" in name or "text_transformer" in name:
-                param.requires_grad = True
-            else:
-                param.requires_grad = False
+            param.requires_grad = True
 
 
         self.val_vqa_loss_list = []
