e6e0558aaaadb8ae84f6d964ecef91dfd7798e22
hoaithi <phunghoaithi137@gmail.com>
2025-03-26 04:49:03 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -330,7 +330,7 @@ _config_vqa_vast = {
     'learning_rate': 5e-5,
     'val_check_interval': 0.1,
     'lr_adapter': 5,
-    'lr_mult_head': 15,
+    'lr_mult_head': 10,
     'lr_mult_cross_modal': 10, # 5
     'max_epoch': 5,
     'max_steps': 27000,
