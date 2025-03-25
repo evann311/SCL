e0df1823c8ff16aee008d986b4164321dd1d896d
hoaithi <phunghoaithi137@gmail.com>
2025-03-25 09:34:14 +0700

    update

--- run.py
+++ run.py
@@ -75,91 +75,78 @@ if __name__ == '__main__':
     _config = copy.deepcopy(config_dict[config.task])
     pl.seed_everything(_config["seed"])
 
-    from tqdm import tqdm
-
     dm = MTDataModule(_config, dist=True)
-    dataset = dm.train_dataset
-    for i in tqdm(range(len(dataset))):
-        try:
-            item = dataset[i]
-            caption = item.get('text', None) or item.get('caption', None)
-            if caption is None or caption.strip() == "":
-                print(f"❌ Thiếu caption tại index {i}")
-                print(item)
-        except Exception as e:
-            print(f"❌ Lỗi khi đọc sample {i}: {e}")
-    
 
-    # model = SCLTransformer(_config)
-    # exp_name = f'{_config["exp_name"]}'
+    model = SCLTransformer(_config)
+    exp_name = f'{_config["exp_name"]}'
 
-    # os.makedirs(_config["log_dir"], exist_ok=True)
-    # # model save setting
-    # if config.task == 'pretrain': 
-    #     checkpoint_callback = pl.callbacks.ModelCheckpoint(
-    #         save_top_k=5,
-    #         verbose=True,
-    #         monitor="val/the_metric",
-    #         mode="max",
-    #         save_last=True,
-    #         every_n_train_steps=5000, # to save checkpoints each 5k steps according to val metrics
-    #     )
-    #     # checkpoint_callback = pl.callbacks.ModelCheckpoint(
-    #         # every_n_train_steps=500,
-    #    # )
-    # else:
-    #     checkpoint_callback = pl.callbacks.ModelCheckpoint(
-    #         save_top_k=1,
-    #         verbose=True,
-    #         monitor="val/the_metric",
-    #         mode="max",
-    #         save_last=True,
-    #     )
+    os.makedirs(_config["log_dir"], exist_ok=True)
+    # model save setting
+    if config.task == 'pretrain': 
+        checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            save_top_k=5,
+            verbose=True,
+            monitor="val/the_metric",
+            mode="max",
+            save_last=True,
+            every_n_train_steps=5000, # to save checkpoints each 5k steps according to val metrics
+        )
+        # checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            # every_n_train_steps=500,
+       # )
+    else:
+        checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            save_top_k=1,
+            verbose=True,
+            monitor="val/the_metric",
+            mode="max",
+            save_last=True,
+        )
 
 
-    # logger = pl.loggers.TensorBoardLogger(
-    #     _config["log_dir"],
-    #     name=f'{exp_name}_seed{_config["seed"]}_from_{_config["load_path"].split("/")[-1][:-5]}',
-    # )
+    logger = pl.loggers.TensorBoardLogger(
+        _config["log_dir"],
+        name=f'{exp_name}_seed{_config["seed"]}_from_{_config["load_path"].split("/")[-1][:-5]}',
+    )
 
-    # lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
-    # callbacks = [checkpoint_callback, lr_callback]
+    lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
+    callbacks = [checkpoint_callback, lr_callback]
 
-    # num_gpus = (
-    #     _config["num_gpus"]
-    #     if isinstance(_config["num_gpus"], int)
-    #     else len(_config["num_gpus"])
-    # )
+    num_gpus = (
+        _config["num_gpus"]
+        if isinstance(_config["num_gpus"], int)
+        else len(_config["num_gpus"])
+    )
 
-    # grad_steps = _config["batch_size"] // (
-    #     _config["per_gpu_batchsize"] * num_gpus * _config["num_nodes"]
-    # )
+    grad_steps = _config["batch_size"] // (
+        _config["per_gpu_batchsize"] * num_gpus * _config["num_nodes"]
+    )
 
-    # max_steps = _config["max_steps"] if _config["max_steps"] is not None else None
+    max_steps = _config["max_steps"] if _config["max_steps"] is not None else None
 
-    # trainer = pl.Trainer(
-    #     # plugins=[MyCluster(), MyDDPPlugin()], # for multi-machine ddp
-    #     accelerator="gpu" if _config.get("num_gpus", 0) > 0 else "cpu",
-    #     devices=_config.get("num_gpus", 1),
-    #     num_nodes=_config["num_nodes"],
-    #     precision=_config["precision"],
-    #     strategy=DDPStrategy(find_unused_parameters=False),
-    #     benchmark=True,
-    #     deterministic=True,
-    #     max_epochs=_config["max_epoch"] if max_steps is None else 1000,
-    #     max_steps=max_steps,
-    #     callbacks=callbacks,
-    #     logger=logger,
-    #     accumulate_grad_batches=grad_steps,
-    #     log_every_n_steps=10,
-    #     enable_model_summary=True,
-    #     fast_dev_run=_config["fast_dev_run"],
-    #     val_check_interval=_config["val_check_interval"],
-    #     detect_anomaly= True,
-    #     # limit_train_batches=5,
-    #     # limit_val_batches=1
-    # )
-    # if not _config["test_only"]:
-    #     trainer.fit(model, datamodule=dm, ckpt_path=_config.get("resume_from", None))
-    # else:
-    #     trainer.test(model, datamodule=dm)
+    trainer = pl.Trainer(
+        # plugins=[MyCluster(), MyDDPPlugin()], # for multi-machine ddp
+        accelerator="gpu" if _config.get("num_gpus", 0) > 0 else "cpu",
+        devices=_config.get("num_gpus", 1),
+        num_nodes=_config["num_nodes"],
+        precision=_config["precision"],
+        strategy=DDPStrategy(find_unused_parameters=False),
+        benchmark=True,
+        deterministic=True,
+        max_epochs=_config["max_epoch"] if max_steps is None else 1000,
+        max_steps=max_steps,
+        callbacks=callbacks,
+        logger=logger,
+        accumulate_grad_batches=grad_steps,
+        log_every_n_steps=10,
+        enable_model_summary=True,
+        fast_dev_run=_config["fast_dev_run"],
+        val_check_interval=_config["val_check_interval"],
+        detect_anomaly= True,
+        # limit_train_batches=5,
+        # limit_val_batches=1
+    )
+    if not _config["test_only"]:
+        trainer.fit(model, datamodule=dm, ckpt_path=_config.get("resume_from", None))
+    else:
+        trainer.test(model, datamodule=dm)
---
+++ test.py
@@ -0,0 +1,165 @@
+import os
+import copy
+import torch
+torch.set_float32_matmul_precision('high')
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import TQDMProgressBar
+
+
+from scl.config import config_dict
+
+from scl.modules import SCLTransformer
+from scl.datamodules.multitask_datamodule import MTDataModule
+
+from pytorch_lightning.plugins.environments import ClusterEnvironment
+from pytorch_lightning.strategies import DDPStrategy
+
+import torch.distributed as dist
+
+
+
+import argparse
+
+class MyCluster(ClusterEnvironment):
+
+    def creates_children(self) -> bool:
+        # return True if the cluster is managed (you don't launch processes yourself)
+        return True
+
+    def master_address(self):
+        return os.environ['CHIEF_IP']
+
+    def master_port(self) -> int:
+        return int(os.environ["MASTER_PORT"])
+
+    def world_size(self):
+        return int(os.environ['WORLD_SIZE'])
+
+    def global_rank(self) -> int:
+        return int(os.environ['RANK'])
+
+    def local_rank(self) -> int:
+        return int(os.environ['LOCAL_RANK'])
+
+    def node_rank(self) -> int:
+        return int(os.environ["INDEX"])
+
+    def set_global_rank(self, rank: int) -> None:
+        pass
+
+    def set_world_size(self, size: int) -> None:
+        pass
+
+class MyDDPPlugin(DDPStrategy):
+
+    def init_ddp_connection(self, global_rank = None, world_size = None) -> None:
+        master_uri = "tcp://%s:%s" % (os.environ['CHIEF_IP'], os.environ['MASTER_PORT'])
+        dist.init_process_group(
+        backend=self.torch_distributed_backend,
+        init_method=master_uri,
+        world_size=int(os.environ['WORLD_SIZE']),
+        rank=int(os.environ['RANK']),
+        )
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", type=str, default='pretrain')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    config = parse_args()
+    _config = copy.deepcopy(config_dict[config.task])
+    pl.seed_everything(_config["seed"])
+
+    from tqdm import tqdm
+
+    dm = MTDataModule(_config, dist=True)
+    dataset = dm.train_dataset
+    for i in tqdm(range(len(dataset))):
+        try:
+            item = dataset[i]
+            caption = item.get('text', None) or item.get('caption', None)
+            if caption is None or caption.strip() == "":
+                print(f"❌ Thiếu caption tại index {i}")
+                print(item)
+        except Exception as e:
+            print(f"❌ Lỗi khi đọc sample {i}: {e}")
+    
+
+    # model = SCLTransformer(_config)
+    # exp_name = f'{_config["exp_name"]}'
+
+    # os.makedirs(_config["log_dir"], exist_ok=True)
+    # # model save setting
+    # if config.task == 'pretrain': 
+    #     checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    #         save_top_k=5,
+    #         verbose=True,
+    #         monitor="val/the_metric",
+    #         mode="max",
+    #         save_last=True,
+    #         every_n_train_steps=5000, # to save checkpoints each 5k steps according to val metrics
+    #     )
+    #     # checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    #         # every_n_train_steps=500,
+    #    # )
+    # else:
+    #     checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    #         save_top_k=1,
+    #         verbose=True,
+    #         monitor="val/the_metric",
+    #         mode="max",
+    #         save_last=True,
+    #     )
+
+
+    # logger = pl.loggers.TensorBoardLogger(
+    #     _config["log_dir"],
+    #     name=f'{exp_name}_seed{_config["seed"]}_from_{_config["load_path"].split("/")[-1][:-5]}',
+    # )
+
+    # lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
+    # callbacks = [checkpoint_callback, lr_callback]
+
+    # num_gpus = (
+    #     _config["num_gpus"]
+    #     if isinstance(_config["num_gpus"], int)
+    #     else len(_config["num_gpus"])
+    # )
+
+    # grad_steps = _config["batch_size"] // (
+    #     _config["per_gpu_batchsize"] * num_gpus * _config["num_nodes"]
+    # )
+
+    # max_steps = _config["max_steps"] if _config["max_steps"] is not None else None
+
+    # trainer = pl.Trainer(
+    #     # plugins=[MyCluster(), MyDDPPlugin()], # for multi-machine ddp
+    #     accelerator="gpu" if _config.get("num_gpus", 0) > 0 else "cpu",
+    #     devices=_config.get("num_gpus", 1),
+    #     num_nodes=_config["num_nodes"],
+    #     precision=_config["precision"],
+    #     strategy=DDPStrategy(find_unused_parameters=False),
+    #     benchmark=True,
+    #     deterministic=True,
+    #     max_epochs=_config["max_epoch"] if max_steps is None else 1000,
+    #     max_steps=max_steps,
+    #     callbacks=callbacks,
+    #     logger=logger,
+    #     accumulate_grad_batches=grad_steps,
+    #     log_every_n_steps=10,
+    #     enable_model_summary=True,
+    #     fast_dev_run=_config["fast_dev_run"],
+    #     val_check_interval=_config["val_check_interval"],
+    #     detect_anomaly= True,
+    #     # limit_train_batches=5,
+    #     # limit_val_batches=1
+    # )
+    # if not _config["test_only"]:
+    #     trainer.fit(model, datamodule=dm, ckpt_path=_config.get("resume_from", None))
+    # else:
+    #     trainer.test(model, datamodule=dm)
