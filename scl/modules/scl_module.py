241f079afb3ae0cc3daebb75b68507315e8f2854
hoaithi <phunghoaithi137@gmail.com>
2025-04-04 20:16:35 +0700

    update

--- scl/modules/scl_module.py
+++ scl/modules/scl_module.py
@@ -152,9 +152,10 @@ class SCLTransformer(pl.LightningModule):
         # ===================== freeze ======================
         for name, param in self.named_parameters():
             if 'cross_modal_text_transform' in name or 'cross_modal_image_transform' in name or \
-                'cross_modal_text_layers' in name or 'cross_modal_image_layers' in name or \
-                'cross_modal_text_pooler' in name or 'cross_modal_image_pooler' in name or \
-                'token_type_embeddings' in name or 'vqa_classifier' in name:
+                'vqa_classifier' in name:
+                # 'cross_modal_text_layers' in name or 'cross_modal_image_layers' in name or \
+                # 'cross_modal_text_pooler' in name or 'cross_modal_image_pooler' in name or \
+                # 'token_type_embeddings' in name or 'vqa_classifier' in name:
                 param.requires_grad = True
             else:
                 param.requires_grad = False
