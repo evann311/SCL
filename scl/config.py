77e6d7698bf21ac920c97cdd2ca9a60e233b65ab
hoaithi <phunghoaithi137@gmail.com>
2025-04-23 20:37:38 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -198,7 +198,7 @@ _config_vqa_kg = {
         "con": 0,
         "scl": 0,
         },
-    'batch_size': 72,  # this is a desired batch size; pl trainer will accumulate gradients when per step batch is smaller.
+    'batch_size': 32,  # this is a desired batch size; pl trainer will accumulate gradients when per step batch is smaller.
 
     # Image setting
     'image_size': 384,
@@ -252,7 +252,7 @@ _config_vqa_kg = {
     # below params varies with the environment
     'data_root': '/kaggle/input/arrow-vqa2-20k/arrow-vqa2-20k',
     'log_dir': "result",
-    'per_gpu_batchsize': 36,  # you should define this manually with per_gpu_batch_size=#
+    'per_gpu_batchsize': 16,  # you should define this manually with per_gpu_batch_size=#
     'num_gpus': 2,
     'num_nodes': 1,
     'load_path': "/kaggle/input/glscl/pytorch/default/1/GLSCL-100k.ckpt",
