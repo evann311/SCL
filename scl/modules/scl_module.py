33a1b4a87bcd72ebb2ac375c07088703755d6e12
hoaithi <phunghoaithi137@gmail.com>
2025-05-21 09:16:43 +0700

    update

--- scl/modules/scl_module.py
+++ scl/modules/scl_module.py
@@ -352,6 +352,7 @@ class SCLTransformer(pl.LightningModule):
         return ret
 
     def on_test_epoch_end(self, outs=None):
+        print("on_test_epoch_end")
         print(outs)
         if outs is None:
             return
