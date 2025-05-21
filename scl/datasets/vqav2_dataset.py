1d606dbf2010d78dc7d0fb63652a4b634444cfb9
hoaithi <phunghoaithi137@gmail.com>
2025-05-21 08:30:22 +0700

    update

--- scl/datasets/vqav2_dataset.py
+++ scl/datasets/vqav2_dataset.py
@@ -22,7 +22,7 @@ class VQAv2Dataset(BaseDataset):
             remove_duplicate=False,
         )
 
-        self.max_test_samples = 1000
+        self.max_test_samples = 5000
         if split == "test":
             self._truncate_test_samples()
         
