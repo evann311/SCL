1d386414fd2b93019e0957002e716a3bae48a875
hoaithi <phunghoaithi137@gmail.com>
2025-05-26 01:50:56 +0700

    update

--- scl/modules/objectives.py
+++ scl/modules/objectives.py
@@ -822,8 +822,8 @@ def arc_test_step(pl_module, batch, output):
 
 
 def vqa_test_wrapup(outs, model_name, log_dir):
-    print(outs)
     rank = torch.distributed.get_rank()
+    print(outs)
     qids, preds = list(), list()
     for out in outs:
         qids += out["qids"]
