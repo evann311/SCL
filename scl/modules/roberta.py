7e611b9a3d02c4bf8e57888573f7ff96ed131b02
hoaithi <phunghoaithi137@gmail.com>
2025-06-25 10:39:25 +0700

    update

--- scl/modules/roberta.py
+++ scl/modules/roberta.py
@@ -28,6 +28,7 @@ class RobertaOutput(nn.Module):
     def forward(self, hidden_states, input_tensor):
         hidden_states = self.dense(hidden_states)
         hidden_states = self.dropout(hidden_states)
+        hidden_states = self.adapter(hidden_states)
         hidden_states = self.LayerNorm(hidden_states + input_tensor)
         return hidden_states
 
