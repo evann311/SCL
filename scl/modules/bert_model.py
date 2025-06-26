fa51e4024c9cf39a340304bf123d62784fdcfaa0
hoaithi <phunghoaithi137@gmail.com>
2025-06-26 17:23:11 +0700

    update

--- scl/modules/bert_model.py
+++ scl/modules/bert_model.py
@@ -388,8 +388,6 @@ class BertSelfOutput(nn.Module):
     def forward(self, hidden_states, input_tensor):
         hidden_states = self.dense(hidden_states)
         hidden_states = self.dropout(hidden_states)
-        if self.adapter is not None:
-            hidden_states = self.adapter(hidden_states)
         hidden_states = self.LayerNorm(hidden_states + input_tensor)
         return hidden_states
 
