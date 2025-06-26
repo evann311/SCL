4d2043972c72e64f69f5db352febff51e2df8075
hoaithi <phunghoaithi137@gmail.com>
2025-06-26 17:05:48 +0700

    update

--- scl/modules/scl_module.py
+++ scl/modules/scl_module.py
@@ -35,7 +35,8 @@ class SCLTransformer(pl.LightningModule):
         hs = self.hparams.config["hidden_size"]
 
         # ===================== Pretrain ===================== #
-        self.text_transformer = build_roberta_model(config)
+        # self.text_transformer = build_roberta_model(config)
+        self.text_transformer = BertModel.from_pretrained(config['roberta_path'])
         self.vision_transformer = build_model(config['vit_path'], resolution_after=config["image_size"])
 
         # ===================== Cross Modal ===================== #
