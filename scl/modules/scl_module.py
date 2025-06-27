0c0991322bb0096c1976422a76edaed73cf80caa
hoaithi <phunghoaithi137@gmail.com>
2025-06-27 12:22:42 +0700

    update

--- scl/modules/scl_module.py
+++ scl/modules/scl_module.py
@@ -315,6 +315,9 @@ class SCLTransformer(pl.LightningModule):
 
         global_step = self.global_step
 
+        device = torch.cuda.current_device()
+        allocated = torch.cuda.memory_allocated(device) / (1024 ** 3)
+        reserved = torch.cuda.memory_reserved(device) / (1024 ** 3)
         if self.logger is not None and hasattr(self.logger, "experiment"):
             self.logger.experiment.add_scalar(f"gpu_{device}/allocated_GB", allocated, global_step)
             self.logger.experiment.add_scalar(f"gpu_{device}/reserved_GB", reserved, global_step)
