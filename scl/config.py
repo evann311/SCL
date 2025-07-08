2d373a8d26dafb80fe88fb5d76806ad66d29e4be
hoaithi <phunghoaithi137@gmail.com>
2025-07-08 15:50:07 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -348,7 +348,7 @@ _config_vqa_vast = {
     'num_gpus': 2,
     'num_nodes': 1,
     'load_path': "/workspace/last.ckpt",
-    'num_workers': 32,
+    'num_workers': 16,
     'precision': '16-mixed',
     'is_pretrain': False,
 
