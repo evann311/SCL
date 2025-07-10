0f96eec8bf1f3d2bb6999f2128a15f1a10dc1137
hoaithi <phunghoaithi137@gmail.com>
2025-07-10 07:47:09 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -348,7 +348,7 @@ _config_vqa_vast = {
     'per_gpu_batchsize': 128,  # you should define this manually with per_gpu_batch_size=#
     'num_gpus': 4,
     'num_nodes': 1,
-    'load_path': "result/fusion-encoder-adapter-2block-test/checkpoints/last.ckpt",
+    'load_path': "/result/fusion-encoder-adapter-2block-test/checkpoints/last.ckpt",
     'num_workers': 32,
     'precision': '16-mixed',
     'is_pretrain': False,
