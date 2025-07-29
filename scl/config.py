e6457627a216ace55086374aaf81786a4777b222
hoaithi <phunghoaithi137@gmail.com>
2025-07-29 14:19:40 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -290,7 +290,7 @@ _config_vqa_vast = {
         "con": 0,
         "scl": 0,
         },
-    'batch_size': 512,  # this is a desired batch size; pl trainer will accumulate gradients when per step batch is smaller.
+    'batch_size': 256,  # this is a desired batch size; pl trainer will accumulate gradients when per step batch is smaller.
 
     # Image setting
     'image_size': 384,
@@ -344,7 +344,7 @@ _config_vqa_vast = {
     # below params varies with the environment
     'data_root': '/workspace/arrow-vqa2-full',
     'log_dir': "result",
-    'per_gpu_batchsize': 512,  # you should define this manually with per_gpu_batch_size=#
+    'per_gpu_batchsize': 256,  # you should define this manually with per_gpu_batch_size=#
     'num_gpus': 1,
     'num_nodes': 1,
     'load_path': "/workspace/glscl/GLSCL-100k.ckpt",
