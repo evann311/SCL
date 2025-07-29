9bc1ec98fff1322e054c5f6ab10e7e7cfde7d497
hoaithi <phunghoaithi137@gmail.com>
2025-07-29 17:20:56 +0700

    update

--- gradient_analysis.py
+++ gradient_analysis.py
@@ -586,9 +586,9 @@ def main():
     _config = copy.deepcopy(config_dict[config_name])
     
     # Modify config for analysis
-    _config["max_steps"] = 1000 # Reduce steps for analysis
+    _config["max_steps"] = 2000 # Reduce steps for analysis
     _config["val_check_interval"] = 200
-    _config["log_every_n_steps"] = 5
+    _config["log_every_n_steps"] = 1
     
     # Configure for 2 GPU setup
     _config["num_gpus"] = 1
