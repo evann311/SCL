c7f45f684a95d5e1a6e7e274f01ee922a3823d51
hoaithi <phunghoaithi137@gmail.com>
2025-07-30 10:57:33 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -345,8 +345,8 @@ _config_vqa_vast = {
     # below params varies with the environment
     'data_root': '/workspace/arrow-vqa2-full',
     'log_dir': "result",
-    'per_gpu_batchsize': 16,  # you should define this manually with per_gpu_batch_size=#
-    'num_gpus': 4,
+    'per_gpu_batchsize': 32,  # you should define this manually with per_gpu_batch_size=#
+    'num_gpus': 2,
     'num_nodes': 1,
     'load_path': "/workspace/glscl/GLSCL-100k.ckpt",
     'num_workers': 32,
