2ac04d011c1f5503a97bf5ed6eddc77f21a9f0f5
hoaithi <phunghoaithi137@gmail.com>
2025-07-30 15:39:03 +0700

    update

--- scl/config.py
+++ scl/config.py
@@ -340,7 +340,7 @@ _config_vqa_vast = {
     # PL Trainer Setting
     'resume_from': None, # load interrupted ckpt
     'fast_dev_run': False, # for debug
-    'test_only': False,
+    'test_only': True,
 
     # below params varies with the environment
     'data_root': '/workspace/arrow-vqa2-full',
@@ -348,7 +348,7 @@ _config_vqa_vast = {
     'per_gpu_batchsize': 32,  # you should define this manually with per_gpu_batch_size=#
     'num_gpus': 2,
     'num_nodes': 1,
-    'load_path': "/home/user/SCL/result/finetune_vqa_randaug_seed0_from_GLSCL-100k/version_1/checkpoints/last.ckpt",
+    'load_path': "/home/user/SCL/result/finetune_vqa_randaug_seed0_from_GLSCL-100k.ckpt/version_1/checkpoints/last",
     'num_workers': 32,
     'precision': '16-mixed',
     'is_pretrain': False,
